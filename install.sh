6079378db2213c77e1f898bd24ed92119746022b
Daniel Malament <daniel.j.malament@gmail.com>
2013-03-20 18:35:15 -0400

    install.sh

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+case "$1" in
+  system)
+    cp aeolus /usr/local/bin/
+    chmod 755 /usr/local/bin/aeolus
+    chown 0:0 /usr/local/bin/aeolus
+    mkdir /etc/aeolus
+    chmod 755 /etc/aeolus
+    chown 0:0 /etc/aeolus
+    cp CONFIG.SAMPLE USAGE /etc/aeolus/
+    chmod 644 /etc/aeolus/CONFIG.SAMPLE /etc/aeolus/USAGE
+    chown 0:0 /etc/aeolus/CONFIG.SAMPLE /etc/aeolus/USAGE
+    ;;
+  user)
+    mkdir ~/bin
+    chmod u=rwx ~/bin
+    cp aeolus ~/bin/
+    chmod u=rwx ~/bin/aeolus
+    mkdir ~/.aeolus
+    chmod u=rwx ~/.aeolus
+    cp CONFIG.SAMPLE USAGE ~/.aeolus/
+    chmod u=rw ~/.aeolus/CONFIG.SAMPLE ~/.aeolus/USAGE
+    ;;
+  *)
+    cat 1>&2 <<-EOF
+
+	Usage:
+
+	  $0 { system | user }
+
+	"system" installs systemwide;
+	"user" installs to the home directory of the current user
+
+	EOF
+    ;;
+esac
