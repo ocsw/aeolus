463267ec7c69fec6ee7fc202996fca49b14149b4
Daniel Malament <daniel.j.malament@gmail.com>
2013-04-07 12:00:53 -0400

    more mkdir -p

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ case "$1" in
     cp aeolus /usr/local/bin/
     chmod 755 /usr/local/bin/aeolus
     chown 0:0 /usr/local/bin/aeolus
-    mkdir /etc/aeolus
+    mkdir -p /etc/aeolus  # don't complain if it exists
     chmod 755 /etc/aeolus
     chown 0:0 /etc/aeolus
     cp CONFIG.SAMPLE USAGE /etc/aeolus/
@@ -17,7 +17,7 @@ case "$1" in
     chmod u=rwx ~/bin
     cp aeolus ~/bin/
     chmod u=rwx ~/bin/aeolus
-    mkdir ~/.aeolus
+    mkdir -p ~/.aeolus  # don't complain if it exists
     chmod u=rwx ~/.aeolus
     cp CONFIG.SAMPLE USAGE ~/.aeolus/
     chmod u=rw ~/.aeolus/CONFIG.SAMPLE ~/.aeolus/USAGE
